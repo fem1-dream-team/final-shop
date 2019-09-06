9618936db627cab4c8dc3afa965a699f3726b9ba
AlinaMgz <alinamgz@ukr.net>
2019-09-06 19:00:23 +0300

    @alinamgz added stylelint for styled-components (css-in-js)

--- client/src/commons/Footer.js
+++ client/src/commons/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import styled from "styled-components";
 import {Container, Grid, Box} from "@material-ui/core";
 
 export const Footer = () => {
@@ -21,14 +22,20 @@ export const Footer = () => {
                     <Box component="p" m={0}> Kyiv </Box>
                 </Box>
             </Container>
-            <Container>
+            <TestContainer>
                 <Box> 24 / 7 </Box>
                 <Box> +38(073)123-45-67 </Box>
                 <Box>
                     <Box component="p" m={0}> Abbey Road, 12 </Box>
                     <Box component="p" m={0}> Kyiv </Box>
                 </Box>
-            </Container>
+            </TestContainer>
         </Grid>
     )
-}
+};
+
+const TestContainer = styled.div`
+  padding: 10px;
+  font-size: 30px;
+  background-color: limegreen;
+`;
