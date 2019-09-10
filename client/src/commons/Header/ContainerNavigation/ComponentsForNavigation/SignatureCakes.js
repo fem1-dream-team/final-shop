3486f45a3af3931b0856eb1c2aadf199a7c79a11
Varvara <52459820+vsabybina7@users.noreply.github.com>
2019-09-10 19:27:08 +0300

    Test marge header to develop (#13)
    * @alinamgz is adding .travis.yml
    
    * Update .travis.yml
    
    * @alinamgz started project sceleton
    
    * @alinamg updated travis.yml
    
    * @alinamgz removed typo in .travis.yml
    
    * @alinamgz and another typo
    
    * @alinamgz and another try woth travis
    
    * @alinamgz and another try with travis
    
    * @alinamgz fnished rewriting classwork
    
    * @alinamgz rewrited .travis.yml
    
    * @alinamgz added setupTest
    
    * @alinamgz adden language to travis.jml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * @alinamgz preparing to pull travis-acuaintance
    
    * @alinamgz moved dependencies for tests to devdependencies
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * @alinamgz added materialUI, started footer, changed travis.yml
    
    * Create a general view of the header
    
    * error correction indicated by Travis CI
    
    * alt
    
    * create account and search
    
    * creat Account and Search
    
    * Search
    
    * material/ico
    
    * )
    
    * Delete .stylelintcache;
    
    * Update .gitignore

---
+++ client/src/commons/Header/ContainerNavigation/ComponentsForNavigation/SignatureCakes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const SignatureCakes = () => {
+	return (
+		<Container>
+            SignatureCakes
+		</Container>
+	)
+}
+const Container = styled.div`
+	display: flex;
+	justify-content: space-around;
+	height: 800px; 
+	color: green;
+    
+`
