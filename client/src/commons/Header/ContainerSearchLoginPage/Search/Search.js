fb5cf9bad1e91f80022134fe4707de92b02e634f
Varvara Shcherbyna <vsabybina7@gmail.com>
2019-09-12 19:39:57 +0300

    style fix

--- client/src/commons/Header/ContainerSearchLoginPage/Search/Search.js
+++ client/src/commons/Header/ContainerSearchLoginPage/Search/Search.js
@@ -4,12 +4,12 @@ import './Search.css'
 export const Search = (props) => {
 	return (
 
-			<div className="d6">
-				<form>
-					<input type="text" placeholder="Search..."/>
-					<div></div>
-				</form>
-			</div>
+		<div className="d6">
+			<form>
+				<input type="text" placeholder="Search..."/>
+				<div></div>
+			</form>
+		</div>
 
 	)
 }
