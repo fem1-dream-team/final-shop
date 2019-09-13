84f23c5a8dc03b67edd0807c4055b27774cf1a1e
pampukhaleo <55058162+pampukhaleo@users.noreply.github.com>
2019-09-13 15:52:15 +0300

    Categories (#17)
    * Categories 2 exmpl added
    
    * Categories 2 exmpl added
    
    * fixed with lints
    
    * Categories added
    
    * categories to merge
    
    * Update index.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import {SimpleSlider} from '../SimpleSlider';
+import React from 'react'
+// import { SimpleSlider } from '../SimpleSlider'
+import { Categories } from '../Categories'
 
 export const Home = () => {
 	return (
 		<div>
 			<h1>Home page</h1>
-			<SimpleSlider/>
+			{/* <SimpleSlider/> */}
+			<Categories/>
 		</div>
 	)
-};
+}
