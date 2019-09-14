1e8228a6ddd235c3e5ea6f2a3c566e2221946250
Varvara Shcherbyna <vsabybina7@gmail.com>
2019-09-14 14:12:17 +0300

    Cakes fix

--- client/src/components/ComponentsForNavigation/Cakes/Cakes.js
+++ client/src/components/ComponentsForNavigation/Cakes/Cakes.js
@@ -3,10 +3,10 @@ import s from './cakes.module.css'
 
 export const Cakes = () => {
 	return (
-			<div className={s.container}>
-				<p className={s.text}>
+		<div className={s.container}>
+			<p className={s.text}>
 					Make a Bittersweet cake part of your celebration tradition – because a party without a cake is just a meeting and share the joy with friends and family!
-				</p>
-			</div>
+			</p>
+		</div>
 	)
 }
