dac98118ed2931761bd0e5620d19bf222b92f1db
rozovyk <48711282+rozovyk@users.noreply.github.com>
2019-09-17 16:28:33 +0300

    Product list page (#21)
    * @alinamgz is adding .travis.yml
    
    * Update .travis.yml
    
    * @alinamgz started project sceleton
    
    * @alinamg updated travis.yml
    
    * @alinamgz removed typo in .travis.yml
    
    * @alinamgz and another typo
    
    * @alinamgz and another try woth travis
    
    * @alinamgz and another try with travis
    
    * @alinamgz fnished rewriting classwork
    
    * @alinamgz rewrited .travis.yml
    
    * @alinamgz added setupTest
    
    * @alinamgz adden language to travis.jml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * @alinamgz preparing to pull travis-acuaintance
    
    * @alinamgz moved dependencies for tests to devdependencies
    
    * @alinamgz added materialUI, started footer, changed travis.yml
    
    * Update README.md
    
    * @alinamgz organized pictures
    
    * Installed backend server
    
    * Added api
    
    * styles edited
    
    * styles edited
    
    * update
    
    * update
    
    * Added categories
    
    * Removed stylelintcache
    
    * develop
    
    * styled edited
    
    * update

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,69 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import 'typeface-roboto'
-import { Home } from './components'
+import {Home, Products} from './components'
+// import {SimpleSlider} from './components'
 import { Footer, Header } from './commons'
-import { Cakes } from './components/ComponentsForNavigation/Cakes/Cakes'
-import { Biscuits } from './components/ComponentsForNavigation/Biscuits'
 import { Cart } from './components/ComponentsForNavigation/Cart'
-import { Cupcakes } from './components/ComponentsForNavigation/Cupcakes'
-import { Desserts } from './components/ComponentsForNavigation/Desserts'
-import { Tarts } from './components/ComponentsForNavigation/Tarts'
+
 import LoginPage from './commons/Header/ContainerSearchLoginPage/LoginPage';
 // import Button from '@material-ui/core/Button';
 import { ContainerNavigation } from './commons/ContainerNavigation/ContainerNavigation'
 
 class App extends Component {
 	componentDidMount () {
-		axios.get('/data')
-			.then(res => axios.post('/postData', res)
-				.then(console.log))
+		// axios.get('http://localhost:3001/api/getData');
 	}
 
-inputChanged = (param) => {
-	console.log(param)
-}
+	inputChanged = (param) => {
+		console.log(param)
+	};
 
-render () {
-	return (
-		<BrowserRouter>
+	render () {
+		return (
+			<BrowserRouter>
+				<div>
+					<Header/>
+					<ContainerNavigation/>
+					<Switch>
+						<Route exact path='/' component={Home}/>
+						<Route path='/home' render={() => <Home/>}/>
+						<Route
+							path='/cakes'
+							component={props => <Products {...props} category={'cakes'} />}
+						/>
+						<Route
+							path='/tarts'
+							component={props => <Products {...props} category={'tarts'} />}
+						/>
 
-			<div>
-				<Header/>
-				<ContainerNavigation/>
-				<Switch>
-					<Route path='/home' component={Home}/>
-					<Route path='/cakes' component={Cakes}/>
-					<Route path='/tarts' component={Tarts}/>
-					<Route path='/cupcakes' component={Cupcakes}/>
-					<Route path='/biscuits' component={Biscuits}/>
-					<Route path='/desserts' component={Desserts}/>
-					<Route path='/cart' render={() => <Cart/>}/>
-					<Route path='/home' render={() => <Cakes/>}/>
-					{/* <Route  path='*' component={Page404}/> */}
-					<Route exect path='/customer' component={LoginPage}/>
-				</Switch>
-				<Footer/>
-			</div>
-		</BrowserRouter>
-	)
-}
+						<Route
+							path='/cupcakes'
+							component={props => <Products {...props} category={'cupcakes'} />}
+						/>
+
+						<Route
+							path='/biscuits'
+							component={props => <Products {...props} category={'biscuits'} />}
+						/>
+
+						<Route
+							path='/desserts'
+							component={props => <Products {...props} category={'desserts'} />}
+						/>
+
+						<Route path='/cart' render={() => <Cart/>}/>
+						{/* <Route  path='*' component={Page404}/> */}
+						<Route exect path='/customer' component={LoginPage}/>
+
+					</Switch>
+
+					<Footer/>
+				</div>
+			</BrowserRouter>
+		)
+	}
 }
 
 export default App
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,5 @@ export {Home} from './Home/Home';
 export {Profile} from './Profile/Profile';
 export {Page404} from './Page404/Page404';
 export {SimpleSlider} from './SimpleSlider';
-export {Categories} from './Categories'
+export {Categories} from './Categories';
 export {Products} from './Products/Products';
