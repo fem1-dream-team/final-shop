6daece19c7ba4e4109b221613dc4fe55dc4dcd05
Varvara Shcherbyna <vsabybina7@gmail.com>
2019-09-24 11:34:31 +0300

    Products

--- client/src/components/BuyProductCart/BuyProductCart.js
+++ client/src/components/BuyProductCart/BuyProductCart.js
@@ -4,9 +4,9 @@ export const BuyProductCart = (props) => {
 
 	return (
 		<div>
-			<p className={s.header} >YOUR ORDER</p>
+			<p className={s.header}>YOUR ORDER</p>
 			<div>
-				<img src={props.image}/>
+				<img src={props.image} alt='image'/>
 				<h3>{props.name}</h3>
 				<p>{props.description}</p>
 
