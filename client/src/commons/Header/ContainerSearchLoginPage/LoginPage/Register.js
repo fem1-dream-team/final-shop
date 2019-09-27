64cd39747d5ce3fb7a1cc140a37b9b88b8b035f3
AlinaMgz <alinamgz@ukr.net>
2019-09-27 16:30:22 +0300

    now user can see we don't want to accept registrattion form

--- client/src/commons/Header/ContainerSearchLoginPage/LoginPage/Register.js
+++ client/src/commons/Header/ContainerSearchLoginPage/LoginPage/Register.js
@@ -1,21 +1,29 @@
-import React, { useState} from 'react';
-import {withRouter} from 'react-router-dom'
+import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import isEmpty from 'is-empty'
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
 import { registerUser } from '../../../../actions/authActions'
 
 const useStyles = makeStyles(theme => ({
 	button: {
 		margin: theme.spacing(1),
 	},
-}));
+	errorMsg: {
+		marginTop: 0,
+		paddingLeft: 10,
+		paddingRight: 10,
+		fontSize: 12,
+		color: 'red'
+	}
+}))
 
 const Register = (props) => {
-	console.log(props);
+	console.log(props)
 	const [state, setState] = useState({
 		first_name: '',
 		last_name: '',
@@ -23,21 +31,21 @@ const Register = (props) => {
 		password: '',
 		confirm_password: '',
 		errors: {}
-	});
+	})
 
-	const classes = useStyles();
+	const classes = useStyles()
 
 	const onChangeHandler = event => {
-		const name = event.target.getAttribute('name');
-		const value = event.target.value;
+		const name = event.target.getAttribute('name')
+		const value = event.target.value
 		setState({
 			...state,
 			[name]: value,
-		});
-	};
+		})
+	}
 
 	function handleSubmit (event) {
-		event.preventDefault();
+		event.preventDefault()
 
 		const newUser = {
 			first_name: state.first_name,
@@ -45,91 +53,110 @@ const Register = (props) => {
 			email: state.email,
 			password: state.password,
 			confirm_password: state.confirm_password
-		};
+		}
 
-		props.registerUser(newUser);
+		props.registerUser(newUser)
 	}
+
+	const errors = props.errors
+	isEmpty(errors) ? console.log('empty') : console.warn(errors)
 
 	return (
 		<div>
 			<h2>Create your account</h2>
-			<div> Please enter your register details </div>
+			<div> Please enter your register details</div>
 			<form noValidate onSubmit={handleSubmit}>
-				<TextField
-					margin="dense"
-					name="first_name"
-					id="first_name"
-					placeholder="First Name"
-					type="text"
-					fullWidth
-					variant="outlined"
-					value={state.first_name}
-					error={state.errors.first_name}
-					onChange={onChangeHandler}
-				/>
-				<TextField
-					margin="dense"
-					name="last_name"
-					id="last_name"
-					placeholder="Last Name"
-					type="text"
-					fullWidth
-					variant="outlined"
-					value={state.last_name}
-					error={state.errors.last_name}
-					onChange={onChangeHandler}
-				/>
-				<TextField
-					margin="dense"
-					name="email"
-					id="email"
-					placeholder="Email address"
-					type="email"
-					fullWidth
-					variant="outlined"
-					value={state.email}
-					error={state.errors.email}
-					onChange={onChangeHandler}
-				/>
-				<TextField
-					margin="dense"
-					name="password"
-					id="password"
-					placeholder="Password. Minimum 8 characters"
-					type="password"
-					fullWidth
-					variant="outlined"
-					value={state.password}
-					error={state.errors.password}
-					onChange={onChangeHandler}
-				/>
-				<TextField
-					margin="dense"
-					name="confirm_password"
-					id="confirmPassword"
-					placeholder="Confirm Password"
-					type="password"
-					fullWidth
-					variant="outlined"
-					value={state.confirm_password}
-					error={state.errors.confirm_password}
-					onChange={onChangeHandler}
-				/>
-				<Button type='submit' variant="outlined" color="primary" className={classes.button}> Create your account</Button>
+				<div>
+					<TextField
+						margin="dense"
+						name="first_name"
+						id="first_name"
+						placeholder="First Name"
+						type="text"
+						fullWidth
+						variant="outlined"
+						value={state.first_name}
+						error={state.errors.first_name}
+						onChange={onChangeHandler}
+					/>
+					{errors.first_name ? <p className={classes.errorMsg}> {errors.first_name} </p> : null}
+				</div>
+				<div>
+					<TextField
+						margin="dense"
+						name="last_name"
+						id="last_name"
+						placeholder="Last Name"
+						type="text"
+						fullWidth
+						variant="outlined"
+						value={state.last_name}
+						error={state.errors.last_name}
+						onChange={onChangeHandler}
+					/>
+					{errors.last_name ? <p className={classes.errorMsg}> {errors.last_name} </p> : null}
+				</div>
+				<div>
+					<TextField
+						margin="dense"
+						name="email"
+						id="email"
+						placeholder="Email address"
+						type="email"
+						fullWidth
+						variant="outlined"
+						value={state.email}
+						error={state.errors.email}
+						onChange={onChangeHandler}
+					/>
+					{errors.email ? <p className={classes.errorMsg}> {errors.email} </p> : null}
+				</div>
+				<div>
+					<TextField
+						margin="dense"
+						name="password"
+						id="password"
+						placeholder="Password. Minimum 8 characters"
+						type="password"
+						fullWidth
+						variant="outlined"
+						value={state.password}
+						error={state.errors.password}
+						onChange={onChangeHandler}
+					/>
+					{errors.password ? <p className={classes.errorMsg}> {errors.password} </p> : null}
+				</div>
+				<div>
+					<TextField
+						margin="dense"
+						name="confirm_password"
+						id="confirmPassword"
+						placeholder="Confirm Password"
+						type="password"
+						fullWidth
+						variant="outlined"
+						value={state.confirm_password}
+						error={state.errors.confirm_password}
+						onChange={onChangeHandler}
+					/>
+					{errors.confirm_password ? <p className={classes.errorMsg}> {errors.confirm_password} </p> : null}
+				</div>
+				<Button type='submit' variant="outlined" color="primary" className={classes.button}> Create your
+					account</Button>
 			</form>
 		</div>
-	);
-};
+	)
+}
 
 Register.propTypes = {
 	registerUser: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	errors: PropTypes.object.isRequired,
-};
+}
 
 const mapStateToProps = state => ({
 	auth: state.auth,
 	errors: state.errors
-});
+})
 
-export default connect(mapStateToProps, {registerUser})(withRouter(Register));
+export default connect(mapStateToProps, { registerUser })(withRouter(Register))
