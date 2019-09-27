24704f852676e0a87ea7cb269c82727a5ef1757b
Alina Mazur <54025456+Alinamgz@users.noreply.github.com>
2019-09-27 23:12:43 +0300

    Auth saga (#32)
    * change localhost adress from 9000 to 3000 in package.json
    
    * raw changes
    
    * update 1
    
    * Update 3
    
    * Update 4
    
    * update bla bla bla
    
    * update. CSS Component
    
    * Update
    
    * @alinamgz updated backend folder: added register api
    
    * @alinamgz almost finished registration. Have to find out how to show validation error
    
    * @alinamgz fixed what eslint found unbeautiful
    
    * @alinamgz edited registerUser post uri
    
    * @alinamgz added redux-saga to auth-modal-window, added saga to register submit, have to figure out how to change register to login if registration went good
    
    * @alinamgz rewrites auth and register form with sagas. when register is ok - login form is shown
    
    * @alinamgz fixed what eslin unbeautiful found
    
    * Delete .stylelintcache
    
    * Update .gitignore
    
    * Update types.js
    
    * Update authActions.js
    
    * @alinamz fixed createNewUser uri
    
    * @alinamgz fixed error msg if registration form filled incorrectly
    
    * need to pull develop
    
    * now user can see we don't want to accept registrattion form
    
    * @alinamgz made some backend for login
    
    * @alinamgz started working on login

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 import { call, put, takeEvery} from 'redux-saga/effects'
-import { GET_ERRORS, POST_NEW_USER } from './types'
+import { GET_ERRORS, LOGIN_USER, POST_NEW_USER } from './types'
 import { showSagaLogin } from './sagaAuthForm'
 
 export const registerUser = (userData) => {
@@ -10,23 +11,47 @@ export const registerUser = (userData) => {
 	}
 }
 
-const registerFailed = (err) => {
+export const loginUserAction = (userToLogin) => {
+	return {
+		type: LOGIN_USER,
+		payload: userToLogin
+	}
+}
+
+const authFailed = (err) => {
 	return {
 		type: GET_ERRORS,
 		payload: err
 	}
 }
 
-function * createNewUser (action) {
+function * createNewUserWorker (action) {
 	try {
 		yield call(() => axios.post('http://localhost:3001/api/register', action.payload));
 		yield put(showSagaLogin())
 	} catch (err) {
-		console.log(yield err)
-		yield put(registerFailed(err))
+		console.error(err.response.data)
+		yield put(authFailed(err.response.data))
 	}
 }
 
-export function * postNewUserSaga () {
-	yield takeEvery(POST_NEW_USER, createNewUser)
+function * loginUserWorker (action) {
+//	TODO
+	try {
+		const response = yield call(() => axios.post('http://localhost:3001/api/login', action.payload));
+		const decoded = jwt_decode(response.data.token)
+		yield localStorage.setItem('jwtToken', response.data.token)
+		// yield localStorage.setItem('expirationDate', decoded.exp)
+		// yield localStorage.setItem('userId', decoded.id)
+		yield console.log('welcome, ' + decoded.first_name)
+		// debugger
+	} catch (err) {
+		console.log(err.response.data)
+		yield put(authFailed(err.response.data))
+	}
+}
+
+export function * watchNewUserSaga () {
+	yield takeEvery(POST_NEW_USER, createNewUserWorker)
+	yield takeEvery(LOGIN_USER, loginUserWorker)
 }
--- client/src/commons/Header/ContainerSearchLoginPage/LoginPage/Login.js
+++ client/src/commons/Header/ContainerSearchLoginPage/LoginPage/Login.js
@@ -1,70 +1,106 @@
-import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import { connect } from 'react-redux'
+import isEmpty from 'is-empty'
+import { loginUserAction } from '../../../../actions/authActions'
 
 const useStyles = makeStyles(theme => ({
 	button: {
 		margin: theme.spacing(1),
 	},
+	errorMsg: {
+		marginTop: 0,
+		paddingLeft: 10,
+		paddingRight: 10,
+		fontSize: 12,
+		color: 'red'
+	}
 
-}));
-export const Login = (props) => {
-	const classes = useStyles();
+}))
+const Login = (props) => {
+	const classes = useStyles()
 	const [state, setState] = useState({
 		email: '',
 		password: '',
-		formErrors: {email: '', password: ''},
-		emailValid: false,
-		passwordValid: false,
-		formValid: false
-	});
+		// formErrors: { email: '', password: '' },
+		// emailValid: false,
+		// passwordValid: false,
+		// formValid: false,
+		errors: {}
+	})
 
 	const onChangeHandler = event => {
-		const name = event.target.getAttribute('name');
-		const value = event.target.value;
+		const name = event.target.getAttribute('name')
+		const value = event.target.value
 		setState({
 			...state,
 			[name]: value,
-		});
-	};
-
-	const onSubmit = () => {
-		console.log(state);
+		})
 	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+
+		const userToLogin = {
+			email: state.email,
+			password: state.password
+		}
+
+		console.log(userToLogin)
+		props.loginUserAction(userToLogin)
+	}
+
+	const errors = props.errors
+	isEmpty(errors) ? console.log('empty') : console.warn(errors)
 
 	return (
 		<div>
 			<h2>Log In</h2>
-			<div> Please enter your account details </div>
-			<form>
-				<TextField
-					autoFocus
-					margin="dense"
-					name="email"
-					id="email"
-					placeholder="Email address"
-					type="email"
-					fullWidth
-					variant="outlined"
-					value={state.email}
-					onChange={onChangeHandler}
-				/>
-				<TextField
-					margin="dense"
-					name="password"
-					id="password"
-					placeholder="Password"
-					type="password"
-					fullWidth
-					variant="outlined"
-					value={state.password}
-					onChange={onChangeHandler}
-				/>
+			<div> Please enter your account details</div>
+			<form noValidate onSubmit={onSubmit}>
 				<div>
+					<TextField
+						autoFocus
+						margin="dense"
+						name="email"
+						id="email"
+						placeholder="Email address"
+						type="email"
+						fullWidth
+						variant="outlined"
+						value={state.email}
+						onChange={onChangeHandler}
+					/>
+					{errors.email ? <p className={classes.errorMsg}> {errors.email} </p> : null}
+				</div>
+				<div>
+					<TextField
+						margin="dense"
+						name="password"
+						id="password"
+						placeholder="Password"
+						type="password"
+						fullWidth
+						variant="outlined"
+						value={state.password}
+						onChange={onChangeHandler}
+					/>
+					{errors.password ? <p className={classes.errorMsg}> {errors.password} </p> : null}
 				</div>
 				<Button variant="outlined" color="primary" className={classes.button} onClick={onSubmit}>Log In</Button>
 			</form>
 		</div>
-	);
-};
+	)
+}
+
+const mapStateToProps = (state) => {
+	return {
+		auth: state.auth,
+		errors: state.errors
+	}
+}
+// export default Login
+export default connect(mapStateToProps, {loginUserAction})(Login)
+// export default connect(mapStateToProps, {})(Login)
